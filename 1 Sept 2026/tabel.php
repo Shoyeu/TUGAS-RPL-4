a7ad2837573a6578ce4e088a65fa58e33bfa0d1b
Shoyeu <kardashdev.main@gmail.com>
2026-09-01 12:56:57 +0700

    Create tabel.php

---
+++ 1 Sept 2026/tabel.php
@@ -0,0 +1,55 @@
+<?php include "navbar.php"; ?>
+
+<div class="container">
+  <div class="row align-items-start">
+    <div class="col-2">
+      <!-- One of three columns -->
+    </div>
+    <div class="col-8">
+      <!-- awal card -->
+      <div class="card">
+        <div class="card-header">
+          Tabel
+        </div>
+        <div class="card-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Nama Depan</th>
+                <th scope="col">Nama Belakang</th>
+                <th scope="col">Kelas</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Dika</td>
+                <td>Pradiya</td>
+                <td>11 RPL 4</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Akmal</td>
+                <td>Putra</td>
+                <td>11 RPL 4</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Dzian</td>
+                <td>Rayista</td>
+                <td>11 RPL 4</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <!-- akhir card -->
+    </div>
+    <div class="col-2">
+      <!-- One of three columns -->
+    </div>
+  </div>
+</div>
+
+<?php include "script.php"; ?>
